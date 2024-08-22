6de9d394aee015874bb4161136b6ba0f989ef2c4
huy1235588 <viettien4869@gmail.com>
2024-08-22 21:06:38 +0700

    feat(routes): add routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Projects from "./pages/Projects";
 
 export default function App() {
-  return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-out" element={<SignOut />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
