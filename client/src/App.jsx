f5168d700f976cd7df776d4317b4fb9113ce08b6
huy1235588 <viettien4869@gmail.com>
2024-08-22 23:14:11 +0700

    feat: add header component using Flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/about" element={<About />} />
                 <Route path="/" element={<Home />} />
